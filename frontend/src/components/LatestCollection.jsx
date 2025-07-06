5cbeda5c1fc97be6e70f141fbd60d415abc58d81
milancodess <milanbhandari153@gmail.com>
2025-07-06 23:21:41 +0545

    Implement LatestCollection component with product rendering and add Title and ProductItem components

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,11 +1,42 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
-  console.log(products);
+  const [latestProducts, setLatestProducts] = useState([]);
 
-  return <div></div>;
+  useEffect(() => {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1={"LATEST"} text2={"COLLECTION"} />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam
+          nesciunt hic iure a cumque ab vel officiis architecto, molestias
+          corrupti animi laborum labore placeat inventore blanditiis, fugit
+          rerum impedit sunt.
+        </p>
+      </div>
+
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LatestCollection;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,9 @@ export const Navbar = () => {
 
   return (
     <div className="relative flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo} className="w-36" alt="Logo" />
+      <Link to="/">
+        <img src={assets.logo} className="w-36" alt="Logo" />
+      </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         {["/", "/collection", "/about", "/contact"].map((path, idx) => {
