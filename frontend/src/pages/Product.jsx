8c02cf72138cbe3aa495c5a694f76fcf29b2df39
milancodess <milanbhandari153@gmail.com>
2025-07-07 20:39:59 +0545

    Add RelatedProducts component to display related items on Product page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -82,8 +83,39 @@ const Product = () => {
             ADD TO CART
           </button>
           <hr className="mt-8 sm:w-4/5" />
+          <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p>Easy return and exchange policy within 7 days.</p>
+          </div>
         </div>
       </div>
+      {/* -------------Description and Review Section----------------- */}
+      <div className="mt-20">
+        <div className="flex">
+          <b className="border px-5 py-3 text-sm">Description</b>
+          <p className="border px-5 py-3 text-sm">Reviews(122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti
+            dolor blanditiis, quidem quos saepe perferendis iusto delectus,
+            consectetur voluptatum nihil, eligendi eveniet esse. Id possimus
+            accusantium et saepe debitis! Molestiae.
+          </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt
+            ipsam deleniti illo quidem accusantium aliquam, qui amet mollitia.
+            Deserunt quibusdam saepe, repudiandae sunt nulla minima expedita
+            eum? Sequi, tenetur voluptatibus!
+          </p>
+        </div>
+      </div>
+      {/* ---------Display Related Products -------------- */}
+      <RelatedProducts
+        category={productData.category}
+        subCategory={productData.subCategory}
+      />
     </div>
   ) : (
     <div className="opacity-0"></div>
