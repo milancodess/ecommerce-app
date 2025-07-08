289c0a1c81ff95e96882027bb012426beb26775e
milancodess <milanbhandari153@gmail.com>
2025-07-08 18:57:00 +0545

    Add product management functionality: implement product routes and controller for adding, listing, and removing products, and integrate multer for file uploads.

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
 
 // App Config
 const app = express();
@@ -11,13 +12,14 @@ const port = process.env.PORT || 4000;
 
 // Connect to DB and Cloudinary
 connectDB();
-connectCloudinary(); // ← was missing ()
+connectCloudinary();
 
 app.use(express.json());
 app.use(cors());
 
 // API endpoints
 app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
