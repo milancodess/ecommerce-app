63eb81589b0fbdd03d1c547e16f99638d90712f5
milancodess <milanbhandari153@gmail.com>
2025-07-08 18:08:55 +0545

    Implement user login functionality in userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,24 @@ const createToken = (id) => {
 
 // Route for user login
 const loginUser = async (req, res) => {
-  // to be implemented
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User doesn't exists." });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      const token = createToken(user._id);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
+  }
 };
 
 // Route for user register
