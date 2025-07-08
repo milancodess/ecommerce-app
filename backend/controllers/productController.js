e097bc2cac5cde6b97484d1669d423c57ae30985
milancodess <milanbhandari153@gmail.com>
2025-07-08 19:17:26 +0545

    Implement single product retrieval in productController and admin login functionality in userController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -83,6 +83,15 @@ const removeProduct = async (req, res) => {
 };
 
 //  Function for single product info
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { addProduct, listProduct, removeProduct, singleProduct };
