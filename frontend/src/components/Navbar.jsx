d2646c9b578f9945168eaaaea0161cda7ba4b5d1
milancodess <milanbhandari153@gmail.com>
2025-07-08 10:27:47 +0545

    Enhance Login page: implement form with email and password fields, toggle between Login and Sign Up states, and add a "Forgot your password?" link.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -47,11 +47,13 @@ export const Navbar = () => {
         />
 
         <div className="group relative">
-          <img
-            className="w-5 cursor-pointer"
-            src={assets.profile_icon}
-            alt="Profile"
-          />
+          <Link to="/login">
+            <img
+              className="w-5 cursor-pointer"
+              src={assets.profile_icon}
+              alt="Profile"
+            />
+          </Link>
           <div className="hidden group-hover:block absolute right-0 pt-4 z-10">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
