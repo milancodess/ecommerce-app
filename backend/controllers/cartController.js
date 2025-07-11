ababcf6d18f354ea2e43f846795b8af2411ce958
milancodess <milanbhandari153@gmail.com>
2025-07-11 13:07:35 +0545

    Add to card

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,35 @@
+// Add products to user cart
+
+import { response } from "express";
+import userModel from "../models/userModel.js";
+
+const addToCart = async (req, res) => {
+  try {
+    const { userId, itemId, size } = req.body;
+    const userData = await userModel.findById(userId);
+    let cartData = userData.cartData;
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+    await userModel.findByIdAndUpdate(userId, { cartData });
+    response.json({ success: true, message: "Added to cart" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Update user cart
+const updateCart = async (req, res) => {};
+
+// Get user cart data
+const getUserCart = async (req, res) => {};
+
+export { addToCart, updateCart, getUserCart };
