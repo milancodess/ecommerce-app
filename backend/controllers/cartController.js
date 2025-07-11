92b5ad29d0401a1a8cf4865ff7b91c7c836837e2
milancodess <milanbhandari153@gmail.com>
2025-07-11 13:10:19 +0545

    Logic for updateCart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -19,7 +19,7 @@ const addToCart = async (req, res) => {
       cartData[itemId][size] = 1;
     }
     await userModel.findByIdAndUpdate(userId, { cartData });
-    response.json({ success: true, message: "Added to cart" });
+    res.json({ success: true, message: "Added to cart." });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
@@ -27,7 +27,16 @@ const addToCart = async (req, res) => {
 };
 
 // Update user cart
-const updateCart = async (req, res) => {};
+const updateCart = async (req, res) => {
+  try {
+    const { userId, itemId, size, quantity } = req.body;
+    const userData = await userModel.findById(userId);
+    let cartData = userData.cartData;
+    cartData[itemId][size] = quantity;
+    await userModel.findByIdAndUpdate(userId, { cartData });
+    res.json({ success: true, message: "Cart updated." });
+  } catch (error) {}
+};
 
 // Get user cart data
 const getUserCart = async (req, res) => {};
