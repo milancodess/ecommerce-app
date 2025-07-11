71c77c68f614632282abadb55799e48ecd1179e9
milancodess <milanbhandari153@gmail.com>
2025-07-11 13:13:43 +0545

    Logic for getUserCart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -35,10 +35,23 @@ const updateCart = async (req, res) => {
     cartData[itemId][size] = quantity;
     await userModel.findByIdAndUpdate(userId, { cartData });
     res.json({ success: true, message: "Cart updated." });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
 // Get user cart data
-const getUserCart = async (req, res) => {};
+const getUserCart = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const userData = await userModel.findById(userId);
+    let cartData = userData.cartData;
+    res.json({ success: true, cartData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { addToCart, updateCart, getUserCart };
